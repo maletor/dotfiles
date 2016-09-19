f6f315f4cdb6b92b97623a2b5f46f8ba8d4fcab5
Ellis Berner <bernee@alum.rpi.edu>
2016-09-19 13:21:34 -0700

    fish: add branch to prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,5 +1,5 @@
 function fish_prompt
   test $SSH_TTY; and printf (set_color red)(whoami)(set_color white)'@'(set_color yellow)(hostname)' '
   test $USER = 'root'; and echo (set_color red)"#"
-  echo -n (set_color cyan)(prompt_pwd) (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
+  echo -n (set_color cyan)(prompt_pwd) (set_color magenta)(git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||') (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
 end

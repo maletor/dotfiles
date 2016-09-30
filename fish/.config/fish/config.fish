5a0c0c9dd16b625e0630b9b37de831a1b558f167
Ellis Berner <bernee@alum.rpi.edu>
2016-09-30 10:57:44 -0700

    less: add essential options

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,6 @@
 set -x EDITOR vim
 set -x PAGER less
+set -x LESS '-F -i -J -M -R -W -x4 -X -z-4'
 
 set fish_user_paths /Users/maletor/.gem/ruby/2.3.0/bin /Users/maletor/.local/bin
 set fish_greeting

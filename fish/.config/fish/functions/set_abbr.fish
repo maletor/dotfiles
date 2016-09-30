9645b3c06ab3b7caa74cda6da43c5ccd85131bc4
Ellis Berner <bernee@alum.rpi.edu>
2016-09-29 22:10:11 -0700

    fish: abbr

---
+++ fish/.config/fish/functions/set_abbr.fish
@@ -0,0 +1,31 @@
+function set_abbr
+    abbr b bundle
+    abbr be bundle exec
+    abbr e vim
+    abbr et ember test --server
+    abbr g git
+    abbr ga git aa
+    abbr gb git branch
+    abbr gc git commit -v
+    abbr gca git commit -a -v
+    abbr gco git checkout
+    abbr gd git diff
+    abbr gdc git diff --cached
+    abbr gds git diff --stat
+    abbr gl git pull --prune
+    abbr glog "git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+    abbr gp git push
+    abbr gpf git push --force-with-lease
+    abbr grc git rebase --continue
+    abbr grep grep --ignore-case --color
+    abbr gri git rebase -i origin/master
+    abbr gs git status -sb
+    abbr ll ls -al
+    abbr ln ln -v
+    abbr mkdir mkdir -p
+    abbr nombom rm -rf {bower_components,dist,node_modules,tmp}
+    abbr rk bundle exec rake
+    abbr s bundle exec rspec
+    abbr ta tmux a
+    abbr tn tmux new
+end

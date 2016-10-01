6b4fd06f959daaec61fb730464d25825f8b853c5
Ellis Berner <bernee@alum.rpi.edu>
2016-09-30 18:41:01 -0700

    fish: add utf8

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,5 +3,5 @@ set -x PAGER less
 set -x LESS '-F -i -J -M -R -W -x4 -X -z-4'
 set -x GREP_OPTIONS '--ignore-case --color'
 
-set fish_user_paths /Users/maletor/.gem/ruby/2.3.0/bin /Users/maletor/.local/bin
+set fish_user_paths $HOME/.gem/ruby/2.3.0/bin $HOME/.local/bin
 set fish_greeting

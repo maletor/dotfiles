687f66b0cb7a04dc66ecb8d2746a0d88b605ed01
Ellis Berner <bernee@alum.rpi.edu>
2016-10-07 09:31:04 -0700

    fish: add configuration options

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,8 @@
 set -x EDITOR vim
 set -x PAGER less
 set -x LESS '-F -i -J -M -R -W -x4 -X -z-4'
-set -x GREP_OPTIONS '--ignore-case --color'
 
+set fish_color_command cyan
+set fish_color_param magenta
 set fish_user_paths $HOME/.gem/ruby/2.3.0/bin $HOME/.local/bin
 set fish_greeting

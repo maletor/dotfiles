551dd895ce2dbd93ca922acdf01d24b9ac01cb47
Ellis Berner <bernee@alum.rpi.edu>
2016-12-29 11:14:23 -0800

    weechat: remove

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,7 +4,7 @@ set -x LESS '-F -i -J -M -R -W -x4 -X -z-4'
 
 set fish_color_command cyan
 set fish_color_param magenta
-set fish_user_paths $HOME/.gem/ruby/2.3.0/bin $HOME/.local/bin
+set fish_user_paths $HOME/.gem/ruby/2.2.0/bin $HOME/.local/bin
 set fish_greeting
 set fish_user_abbreviations
 

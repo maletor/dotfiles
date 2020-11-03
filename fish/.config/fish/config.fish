0a4b8deb808cab0447a5ec0bf52889171051aa3e
Ellis Berner <bernee@alum.rpi.edu>
2020-11-03 09:31:56 -0800

    Update config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,48 +1,36 @@
-set -x EDITOR nvim
-set -x PAGER less
-set -x LESS '-F -i -J -M -R -W -x4 -X -z-4'
+set --export EDITOR vim
+set --export PAGER less
+set --export LESS '-F -i -J -M -R -W -x4 -X -z-4'
 
-set fish_color_command cyan
-set fish_color_param magenta
-set fish_greeting
-set fish_user_abbreviations
-set fish_user_paths ~/.local/bin
+set --global fish_greeting
+set --global fish_user_paths ~/.local/bin
 
-if status --is-interactive
-	set BASE16_SHELL "$HOME/.config/base16-shell/"
-	source "$BASE16_SHELL/profile_helper.fish"
-end
-
-abbr b bundle
-abbr be bundle exec
-abbr buao "brew update; and brew outdated"
-abbr e $EDITOR
-abbr et ember test --server
-abbr g git
-abbr ga git aa
-abbr gb git branch
-abbr gc git commit -v
-abbr gca git commit -a -v
-abbr gco git checkout
-abbr gmen git commit --amend -v
-abbr gd git diff
-abbr gdc git diff --cached
-abbr gds git diff --stat
-abbr gnb git checkout -b
-abbr gl git pull --prune
-abbr glog "git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-abbr gp git push
-abbr gpf git push --force-with-lease
-abbr grc git rebase --continue
-abbr gri git rebase -i origin/master
-abbr gs git status -sb
-abbr ll ls -al
-abbr nombom rm -rf {bower_components,dist,node_modules,tmp}
-abbr rk bundle exec rake
-abbr s bundle exec rspec
-abbr ta tmux a
-abbr tn tmux new -As
-
-abbr ssh-prod-mysql-master ssh -NL 3306:prod-mysql-master.dox.box:3306 eberner@aws-bastion
-abbr ssh-prod-console-1 ssh -NL 2222:prod-doximity-console-1.dox.box:22 eberner@aws-bastion
-abbr mysql-prod mysql -h 0.0.0.0 -P 3306 -p -u eberner -A
+abbr --add --global b bundle
+abbr --add --global be bundle exec
+abbr --add --global buao "brew update; and brew outdated"
+abbr --add --global e $EDITOR
+abbr --add --global et ember test --server
+abbr --add --global g git
+abbr --add --global ga git aa
+abbr --add --global gb git branch
+abbr --add --global gc git commit -v
+abbr --add --global gca git commit -a -v
+abbr --add --global gco git checkout
+abbr --add --global gmen git commit --amend -v
+abbr --add --global gd git diff
+abbr --add --global gdc git diff --cached
+abbr --add --global gds git diff --stat
+abbr --add --global gnb git checkout -b
+abbr --add --global gl git pull --prune
+abbr --add --global glog "git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+abbr --add --global gp git push
+abbr --add --global gpf git push --force-with-lease
+abbr --add --global grc git rebase --continue
+abbr --add --global gri git rebase -i origin/master
+abbr --add --global gs git status -sb
+abbr --add --global ll ls -al
+abbr --add --global nombom rm -rf {bower_components,dist,node_modules,tmp}
+abbr --add --global rk bundle exec rake
+abbr --add --global s bundle exec rspec
+abbr --add --global ta tmux a
+abbr --add --global tn tmux new -As

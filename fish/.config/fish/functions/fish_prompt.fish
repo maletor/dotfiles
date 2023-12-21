fdcc5cb67ab61580ab88dca9e771a47eef1388c1
Ellis Berner <bernee@alum.rpi.edu>
2023-12-21 13:23:24 -0800

    Update fish_prompt.fish

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,3 +1,13 @@
 function fish_prompt
-    echo -n (set_color cyan)(prompt_pwd) (set_color magenta)(git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||') (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
+  set_color cyan
+  printf '%s' (prompt_pwd)
+  set_color normal
+  printf '%s ' (fish_git_prompt)
+
+  set_color red
+  printf '❯'
+  set_color yellow
+  printf '❯'
+  set_color green
+  printf '❯ '
 end
